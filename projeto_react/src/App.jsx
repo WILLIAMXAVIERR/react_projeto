2e3f06cd4dc016c10e486c9338c07ac8a7745a75
WILLIAMXAVIERR <williamrego203@gmail.com>
2024-10-27 22:15:03 -0300

    Criando minha tela de login

---
+++ projeto_react/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Login from './components/login/Login'
+
+function App() {
+
+  return (
+    <>
+    <div className = "App">
+      <p>
+        Página de Login
+        <Login/>
+      </p>
+    </div>
+    </>
+  )
+}
+
+export default App
